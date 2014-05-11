125b8c82a0b6726991f629150315d59faa73d1f8
Ryan Smith <r@32k.io>
2014-05-11 10:24:39 -0700

    Sketch what chain api integration looks like.
    This hasn't been tested.

--- balance_badge.rb
+++ balance_badge.rb
@@ -1,5 +1,28 @@
+require 'net/http'
+require 'json'
 require 'RMagick'
 require 'sinatra'
+
+SATOSHI = 1
+BTC = SATOSHI * 100_000_000
+
+CHAIN_URL = URI.parse(ENV['CHAIN_URL'])
+
+def get_balance(addr_hash)
+  puts addr_hash
+  path = "/bitcoin/addresses/#{addr_hash}"
+  con = Net::HTTP.new(CHAIN_URL.host, CHAIN_URL.port)
+  req = Net::HTTP::Get.new(CHAIN_URL.request_uri + path)
+  req.basic_auth(CHAIN_URL.user, '')
+  resp = con.request(req)
+  return 0 if (Integer(resp.code) / 100) != 2
+  begin
+    json = JSON.parse(resp.body)
+    return Integer(json["balance"]) / BTC
+  rescue
+    return 0
+  end
+end
 
 def gen_badge(balance)
   badge = Magick::ImageList.new("wayne.png")
@@ -18,5 +41,7 @@ end
 get '/address/:addr_hash' do
   content_type 'image/png'
   status(200)
-  body(gen_badge(rand(42)))
+  addr_hash = params['addr_hash']
+  addr_hash.gsub!('.png', '')
+  body(gen_badge(get_balance(addr_hash)))
 end
